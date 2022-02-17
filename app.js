aefd2c3120373d4f2f107d724825d54c8ab687ce
Rachouan Rejeb <info@rachouan.com>
2022-02-17 21:28:36 +0000

    Sync coins from Gecko

--- app.js
+++ app.js
@@ -56,6 +56,9 @@ app.use("/", dashboard);
 const portfolio = require("./routes/portfolio.routes");
 app.use("/portfolio", portfolio);
 
+const coins = require("./routes/coin.routes");
+app.use("/coins", coins);
+
 /* app.get('/assets', (req, res, next) => {
     axios
     .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h')
---
+++ models/Coin.model.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const coinSchema = new Schema(
+  { 
+    id: { type: String, required: true , unique: true},
+    name: String,
+    symbol: String,
+    icon: {type: String, default: '/images/coin/default.jpg'},
+    portfolio: { type: Schema.Types.ObjectId, ref: 'Portfolio' },
+    current_price: Number,
+    market_cap: Number,
+    market_cap_rank: Number,
+    fully_diluted_valuation: Number,
+    total_volume: Number,
+    price_change_percentage: Number,
+    total_supply: Number,
+    circulating_supply: Number,
+    ath: Number,
+    ath_date: String,
+    sparkline_in_7d: Object
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Coin = model("Coin", coinSchema);
+
+module.exports = Coin;
