e28a5e92d5f5dd7f947a94b70809133fe524fa27
Corentin Aesch <corentin.aesch@gmail.com>
2022-02-17 21:10:58 +0100

    Coin model

--- models/marketStat.model.js
+++ models/marketStat.model.js
@@ -1,7 +1,10 @@
 const { Schema, model } = require("mongoose");
 
 const marketStatSchema = new Schema(
-  {
+  { 
+    name: String,
+    symbol: String,
+    icon: {type: String, default: },
     _id: { type: string, required: true },
     currency: { type: Schema.Types.ObjectId, ref: 'Portfolio' },
     price_change_percentage: { type: String, enum: ["1h", "24h", "7d", "14d", "30d", "200d", "1y"] },
