92c420a551f901e6d12e119711860406fbad8a7e
Rachouan Rejeb <info@rachouan.com>
2022-02-17 22:25:54 +0000

    Async Await Coin Data

--- routes/coin.routes.js
+++ routes/coin.routes.js
@@ -3,32 +3,21 @@ const mongoose = require("mongoose");
 const Coin = require("../models/Coin.model.js");
 const axios = require("axios");
 
-/*async do_thing() {
-    const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1000&page=1&sparkline=true&price_change_percentage=24h')
-    const coin_updates = response.data.map(coin => Coin.updateOne({ id: coin.id }, coin, { upsert: true })
-    await Promise.all(coin_updates)
-    const all_coins = await Coin.find()
-    res.json(all_coins)
- } */
-
-/* GET home page */
 router.get("/update", (req, res, next) => {
 
-    
-    axios
-    .get()
-    .then(response => {
-        Promise.all(response.data.map(coin =>{
-            Coin.updateOne({id:coin.id},coin,{upsert: true})
-        }))
-        .then(() => {
-            Coin.find()
-            .then(coins=>res.json(coins))
-            .catch(err=>console.log(err));
-        })
-        .catch(err => console.log(err))
-    })
-    .catch(err => console.log(err));
+    async function getCoins() {
+        try {
+            const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1000&page=1&sparkline=true&price_change_percentage=24h');
+            const coin_updates = response.data.map(coin => Coin.updateOne({ id: coin.id }, coin, { upsert: true }));
+            await Promise.all(coin_updates);
+            const coins = await Coin.find();
+            res.json(coins);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    getCoins();
+   
 });
 
 module.exports = router;
