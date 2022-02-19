b8051b6d8b3fd27b332c4206c539a3929de679d8
Rachouan Rejeb <info@rachouan.com>
2022-02-19 15:32:24 +0000

    Dashboard added portfolios

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const Coin = require("../models/Coin.model.js");
+const Portfolio = require("../models/Portfolio.model.js");
 
 // Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
 const isLoggedOut = require("../middleware/isLoggedOut");
@@ -8,12 +9,19 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 
 /* GET home page */
 router.get("/", isLoggedIn, (req, res, next) => {
-  Coin.find()
-  .then(coins => {
-    console.log(coins);
-    res.render("dashboard",{coins});
-  })
-  .catch(err => console.log(err));
+
+  async function getDashboardData(){
+    try {
+        const coins = await Coin.find()
+        const portfolios = await Portfolio.find()
+        await Promise.all(coins,portfolios);
+        res.render("dashboard",{coins,portfolios});
+    } catch (error) {
+        console.error(error);
+    }
+  }
+  getDashboardData();
+  
 });
 
 module.exports = router;
