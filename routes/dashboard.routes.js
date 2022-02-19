b56c428655a3cf80fb8865adc4e714cf931529a1
Rachouan Rejeb <info@rachouan.com>
2022-02-19 14:49:09 +0000

    Updated Dashboard

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,12 +1,19 @@
 const router = require("express").Router();
 
+const Coin = require("../models/Coin.model.js");
+
 // Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
 /* GET home page */
 router.get("/", isLoggedIn, (req, res, next) => {
-  res.render("dashboard");
+  Coin.find()
+  .then(coins => {
+    console.log(coins);
+    res.render("dashboard",{coins});
+  })
+  .catch(err => console.log(err));
 });
 
 module.exports = router;
