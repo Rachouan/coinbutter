2f2ac566b6fce4d4d5aa2619990c78f7f1c13192
Rachouan Rejeb <info@rachouan.com>
2022-02-22 22:12:04 +0000

    Transaction Auto Fill Amount

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,35 @@
+const transactionForm = document.querySelectorAll('.transactionForm form');
+
+function updateTotal(price,amount,total){
+  const totalValue = price.value * amount.value;
+  total.value = totalValue.toFixed(2);
+}
+
 document.addEventListener(
   "DOMContentLoaded",
   () => {
-    console.log("coinbutter JS imported successfully!");
+    console.log(transactionForm);
+    transactionForm.forEach(form =>{
+      const coinSelection = form.querySelector('.coinSelect');
+      const priceField = form.querySelector('#price');
+      const amountField = form.querySelector('#amount');
+      const totalField = form.querySelector('#total');
+
+      priceField.addEventListener('keyup', (e) => {
+        updateTotal(priceField,amountField,totalField);
+      });
+
+      amountField.addEventListener('keyup', (e) => {
+        updateTotal(priceField,amountField,totalField);
+      });
+
+      coinSelection.addEventListener('change', (e) => {
+        var cp = e.target.options[e.target.selectedIndex].dataset.coinPrice;
+        console.log(cp);
+        priceField.value = cp;
+        updateTotal(priceField,amountField,totalField);
+      });
+    })
   },
   false
 );
