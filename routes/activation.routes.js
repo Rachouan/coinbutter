c1e4ed0ed379917fb6359419a5f0cfec1a60d027
Rachouan Rejeb <info@rachouan.com>
2022-02-26 10:57:08 +0000

    Update Activation

--- routes/activation.routes.js
+++ routes/activation.routes.js
@@ -10,23 +10,25 @@ router.get("/activate", (req, res, next) => {
 
         try{
             const {activate} = req.query;
+            
             if(activate) {
-            }
-            const activateDoc = await Activation.findOne({id:activate})
-            console.log(activateDoc);
-            if(!activateDoc){
-                return res.render('activation/activate',{errors:{message:'You code is not correct.'}});
+                const activateDoc = await Activation.findOne({id:activate})
+                console.log(activateDoc);
+                if(!activateDoc){
+                    return res.render('activation/activate',{errors:{message:'You code is not correct.'}});
+                }
+                
+                await Activation.findByIdAndUpdate({_id:activateDoc._id},{active:true});
+                const user = await User.findOneAndUpdate({_id:activateDoc.user},{active:true});
+                
+                if(req.session.user){
+                    req.session.user.active = true;
+                    res.locals.connectedUser = req.session.user;
+                }
+                
+                res.render('activation/activate');
             }
             
-            await Activation.findByIdAndUpdate({_id:activateDoc._id},{active:true});
-            const user = await User.findOneAndUpdate({_id:activateDoc.user},{active:true});
-            
-            if(req.session.user){
-                req.session.user.active = true;
-                res.locals.connectedUser = req.session.user;
-            }
-            
-            res.render('activation/activate');
             
         }catch(err){
             console.log(err)
