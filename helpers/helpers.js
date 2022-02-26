0787a55ee34a2ec1bdae0c7f168f22cb1f115b1b
Corentin Aesch <corentin.aesch@gmail.com>
2022-02-26 11:57:08 +0100

    asset routes to fic

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -5,6 +5,10 @@ class Helper{
     concatString(str){
         return str.replace(/\s+/g, '-').toLowerCase();
     }
+    round(num) {
+        var m = Number((Math.abs(num) * 100).toPrecision(15));
+        return Math.round(m) / 100 * Math.sign(num);
+    }
 }
 
 const helper = new Helper();
