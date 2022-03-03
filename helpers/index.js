4da4f599277d39a2ac7ccd219f1113914abe2b4a
Corentin Aesch <corentin.aesch@gmail.com>
2022-03-03 18:02:19 +0100

    Updated Asset View

--- helpers/index.js
+++ helpers/index.js
@@ -12,6 +12,8 @@ module.exports = (hbs) => {
       }
   });
   hbs.registerHelper("round2", amount => amount.toFixed(2));
+  hbs.registerHelper("round4", amount => amount.toFixed(4));
   hbs.registerHelper("roundSatochi", amount => amount.toFixed(8));
-
+  hbs.registerHelper("checkIfUpOrDownFormatted", amount => (amount.toFixed().replace(/\B(?=(\d{3})+(?!\d))/g, ',')) > 0 ? 'text-success': 'text-danger');
+  hbs.registerHelper("DateFormater", date => date.toLocaleDateString("fr")); 
 };
